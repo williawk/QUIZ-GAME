11c8938f2eaf0de63a89efeceab1196e4ec8558c
williawk <williawk@gmail.com>
2022-08-05 13:48:59 +0200

    Created quiz code and added problems.csv

--- quiz.go
+++ quiz.go
@@ -1,1 +1,50 @@
-package quiz
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type Question struct {
+	problem string
+	answer  string
+}
+
+var points int
+var totQuestions int
+
+func main() {
+
+	csvFile, err := os.Open("problems.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully opened CSV file")
+	defer csvFile.Close()
+
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, line := range csvLines {
+		question := Question{
+			problem: line[0],
+			answer:  line[1],
+		}
+		askQuestion(question)
+		totQuestions += 1
+	}
+	fmt.Println("You got", points, "/", totQuestions, "questions correct!")
+}
+
+func askQuestion(question Question) {
+	fmt.Println(question.problem)
+	var a1 string
+	fmt.Scanln(&a1)
+
+	if a1 == question.answer {
+		points += 1
+	}
+}
